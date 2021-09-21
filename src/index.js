c133b8c7d4b38465dd87c09e43bb36a489542e58
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-09-21 19:05:03 -0300

    logic of commands done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const { Client } = require('discord.js');
 const myIntents = require('./Helpers/Intents');
-const Interactions = require('./Model/Interactions');
+const ListMethods = require('./Helpers/ListMethods');
 
 const client = new Client({ intents: myIntents });
-const interactions = new Interactions();
+const listMethods = ListMethods;
 
 client.on('ready', () => {
   console.log('oni is hunting another soul ');
@@ -14,21 +14,11 @@ client.on('messageCreate', message => {
   const prefix = message.content.slice(0, 1);
   let messageContent = message.content.split('-');
   messageContent = messageContent[1];
-
   // eslint-disable-next-line no-unused-expressions
-  console.log(prefix, messageContent);
-  console.log(interactions.ListInteractionsMethod);
-  const interationsList = interactions.ListInteractionsMethod;
-  console.log(interationsList.bind(this).call(messageContent));
 
-  /*
-  if (interactions.ListInteractionsMethod[messageContent] && prefix === '-') {
-    const executeMethod = interactions.ListInteractionsMethod.find(message.content);
-    // eslint-disable-next-line no-unused-expressions
-    interactions.ListInteractionsMethod[executeMethod];
-  } else {
-    message.reply('esse contrato não existe ');
-  } */
+  if (listMethods[messageContent] && prefix === '-') {
+    listMethods[messageContent](message);
+  }
 });
 
 client.login(process.env.BOT_KEY);
