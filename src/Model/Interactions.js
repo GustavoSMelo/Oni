b93dd8644c405ccf40309846eebc8f320a8d16f8
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-09-22 20:07:20 -0300

    add function to join in voice channel

--- src/Model/Interactions.js
+++ src/Model/Interactions.js
@@ -1,3 +1,5 @@
+const { joinVoiceChannel } = require('@discordjs/voice');
+
 class Interactions {
   Hello (message) {
     message.reply('hi');
@@ -5,12 +7,18 @@ class Interactions {
 
   async Join (message) {
     try {
-      const voiceChannel = message.member.voice.channel;
-      voiceChannel.join();
+      await joinVoiceChannel({
+        channelId: message.member.voice.channel.id,
+        guildId: message.guild.id,
+        adapterCreator: message.guild.voiceAdapterCreator
+      });
     } catch (err) {
       console.log(err);
       message.reply('Você precisa estar conectado em um canal de audio para poder conectar');
     }
+  }
+
+  async Leave (message) {
   }
 }
 
