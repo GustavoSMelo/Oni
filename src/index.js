735e1ccae5e4ef734548a4fe25974c7441124baf
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-09-22 20:07:32 -0300

    update index code

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ const myIntents = require('./Helpers/Intents');
 const ListMethods = require('./Helpers/ListMethods');
 
 const client = new Client({ intents: myIntents });
-const listMethods = ListMethods;
+const listMethods = ListMethods();
 
 client.on('ready', () => {
   console.log('oni is hunting another soul ');
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async message => {
   const prefix = message.content.slice(0, 1);
   let messageContent = message.content.split('-');
-  messageContent = messageContent[1];
+  messageContent = messageContent[1].toLowerCase();
   let params = '';
   if (message && message.content && message?.content?.split(' ')) {
     params = message.content.split(' ')[1];
