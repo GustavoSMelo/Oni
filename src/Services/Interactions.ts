8e9e224e5b6ff63e2c298b7d851dbeeff548053a
GustavoSMelo <gsantos15569@gmail.com>
2022-05-13 20:48:43 -0300

    create a new interaction

--- src/Services/Interactions.ts
+++ src/Services/Interactions.ts
@@ -1,13 +1,13 @@
 import { Message } from "discord.js";
 
 class Interactions {
-  public Hello (message: Message) {
-    message.reply('hi');
-  }
+    public Hello(message: Message) {
+        message.reply('hi');
+    }
 
-  public Invoke (message: Message, ...params: any) {
-    message.reply(`teste`);
-  }
+    public Invoke(message: Message) {
+        message.reply(`@everyone <@${message.author.id}>`);
+    }
 }
 
 export default Interactions;
