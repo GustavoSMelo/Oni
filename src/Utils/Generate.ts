725bdd1b3476d4021f8b838cf682cdc277a1c7e1
GustavoSMelo <gsantos15569@gmail.com>
2022-06-01 21:36:48 -0300

    change class name

--- src/Utils/Generate.ts
+++ src/Utils/Generate.ts
@@ -1,9 +1,13 @@
-class UtilsGenerate {
-    static generateBetweenTwoSides(sideOne: string, sideTwo:string): string {
+class Generate {
+    public static generateBetweenTwoSides(sideOne: string, sideTwo:string): string {
         const option = Math.round(Math.random());
 
         return option === 0 ? sideOne : sideTwo;
     }
+
+    public static generateARandomNumberBasedOnLength(length: number): number {
+        return Math.round(Math.random() * (length));
+    }
 }
 
-export default UtilsGenerate;
+export default Generate;
