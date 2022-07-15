40c17b17063a6acb85a06c170c7cb16d4241e17b
GustavoSMelo <gsantos15569@gmail.com>
2022-07-15 19:46:40 -0300

    :recycle: resolved method that not decrease a second in cron

--- src/Services/Cron/Cron.ts
+++ src/Services/Cron/Cron.ts
@@ -5,12 +5,18 @@ class Cron {
         this.cronTimer = timer;
     }
 
-    public async decreaseBySecond (): Promise<void> {
-        const timer = new Promise(() => setTimeout(() => this.cronTimer -= 1, 1000));
-        await Promise.all([timer]);
+    public async awaitForTimer (): Promise<void> {
+        const job = setInterval(() => {
+            this.cronTimer -= 1;
+            console.log(this.cronTimer);
+        }, 1000);
+
+        if (this.isFinished()) {
+            clearInterval(job);
+        }
     }
 
-    public isZero (): boolean {
+    public isFinished (): boolean {
         return this.cronTimer > 0 ? false : true;
     }
 }
